0ac6fb49f1f5aa4d979756062f7ad1daa1ed3b83
Jan Klemkow <j.klemkow@wemelug.de>
2013-02-05 12:24:29 +0100

    add polybios decoding

--- polybios.c
+++ polybios.c
@@ -14,9 +14,25 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#define _XOPEN_SOURCE 700
+
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+void
+usage(void)
+{
+	fprintf(stderr, "usage: polybios [-1dh]\n"
+			"\n"
+			"OPTIONS\n"
+			"\t-1\tcoordinates from 1 to 5\n"
+			"\t-d\tdecode\n"
+			"\t-h\tshow this help message\n");
+
+	exit(EXIT_FAILURE);
+}
 
 int
 main(int argc, char**argv)
@@ -28,21 +44,54 @@ main(int argc, char**argv)
 				{'q', 'r', 's', 't', 'u'},
 				{'v', 'w', 'x', 'y', 'z'}};
 
-	while (feof(stdin) == 0) {
-		c = getchar();
-		if (isalpha(c)) {
+	int decode_flag = 0, one_flag, ch;
+
+	while ((ch = getopt(argc, argv, "1dh")) != -1) {
+		switch (ch) {
+		case '1':
+			one_flag = 1;
+			break;
+		case 'd':
+			decode_flag = 1;
+			break;
+		case 'h':
+		default:
+			usage();
+			/* NOTREACHED */
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	while ((c = getchar()) != EOF) {
+		if (!decode_flag && isalpha(c)) {	/* encoding */
 			c = tolower(c);
-			if (c > 'i')
+			if (c > 'i')	/* i and j mapped at the same field */
 				c--;
 
 			x = (c - 'a') % 5;
 			y = (c - 'a') / 5;
 
+			if (one_flag) {
+				x++;
+				y++;
+			}
+
 			printf("%d%d", x, y);
+		} else if (decode_flag && isdigit(c)) {		/* decoding */
+			x = c - '0';
+			y = getchar() - '0';
+
+			if (one_flag) {
+				x--;
+				y--;
+			}
+
+			putchar(table[y][x]);
 		} else	{
 			putchar(c);
 		}
 	}
 
-	return EXIT_FAILURE;
+	return EXIT_SUCCESS;
 }
